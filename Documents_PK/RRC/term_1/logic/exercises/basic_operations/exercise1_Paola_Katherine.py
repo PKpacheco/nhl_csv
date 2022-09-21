bc3f572a9382baa7c28da55116356eb0fd07deee
Paola Katherine <paolakatherine@pk.local>
2022-09-21 10:06:18 -0500

    add ex 1

---
+++ Documents_PK/RRC/term_1/logic/exercises/basic_operations/exercise1_Paola_Katherine.py
@@ -0,0 +1,18 @@
+# Paola Katherine 
+
+
+def calculate_salary(hours, wage):
+    overtime_hours = hours - 40
+    regular_hours = hours - overtime_hours
+    overtime_wage = wage * 1.5
+    regular_pay= regular_hours * wage
+    overtime_pay = overtime_hours * overtime_wage
+    total_pay = regular_pay + overtime_pay
+    return total_pay
+
+
+if __name__ == "__main__":
+    hours = float(input( "Please input the hours:  "))
+    wage = float(input("Please input your wage: "))
+    print_total = calculate_salary(hours, wage)
+    print(" The total pay is : " + str(print_total))
